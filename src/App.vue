23d50a81f1b1b52d6ba5b02d2668ac5b009cc42d
AA12GQ <875151567@qq.com>
2022-05-10 16:05:37 +0800

    Column组件编码与bootstrap美化

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="contarner">
+    <column-list :list="list"></column-list>
+<div class="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 3,
+    title: 'test3的专栏',
+    description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 4,
+    title: 'test4的专栏',
+    description: '这是的test4专栏，有一段非常有意思的简介，可以更新一下欧',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  }
+]
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
   }
 })
 </script>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+    <div v-for="column in list" :key="column.id" class="col-4">
+      <img :src="column.avatar" :alt="column.title">
+      <h5>{{column.title}}</h5>
+      <p>{{column.description}}</p>
+      <a href="#">进入专栏</a>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+
+import { defineComponent, PropType } from 'vue'
+export interface ColumnProps{
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  }
+
+})
+
+</script>
