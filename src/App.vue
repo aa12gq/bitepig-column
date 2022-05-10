70d1deef72ae089ba6f7628da91ad646f9406dcb
AA12GQ <875151567@qq.com>
2022-05-10 16:46:55 +0800

    ColumnList组件编码

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="contarner">
     <column-list :list="list"></column-list>
-<div class="alert alert-primary" role="alert">
-  A simple primary alert—check it out!
-</div>
   </div>
 </template>
 
@@ -21,8 +18,8 @@ const testData: ColumnProps[] = [
   {
     id: 2,
     title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧'
+    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
   },
   {
     id: 3,
