a522dc6dd230b3014700b46c5a21db09b1706325
AA12GQ <875151567@qq.com>
2022-05-11 09:05:21 +0800

    添加DropdownItem组件

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -2,8 +2,7 @@
   <div class="dropdown">
     <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">{{title}}</a>
     <ul class="dropdown-menu" :style="{display:'block'}" v-if="isOpen">
-    <li><a class="dropdown-item" href="#">新建文章</a></li>
-    <li><a class="dropdown-item" href="#">编辑资料</a></li>
+    <slot></slot>
   </ul>
   </div>
 </template>
--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -7,7 +7,11 @@
     </ul>
     <ul v-else class="list-inline mb-0">
       <li class="list-inline-item">
-        <dropdown :title="`你好 ${user.name}`"></dropdown>
+        <dropdown :title="`你好 ${user.name}`">
+          <dropdown-item><a href="#" class="dropdown-item">新建文章</a></dropdown-item>
+          <dropdown-item><a href="#" class="dropdown-item">编辑资料</a></dropdown-item>
+          <dropdown-item><a href="#" class="dropdown-item">退出登陆</a></dropdown-item>
+        </dropdown>
       </li>
     </ul>
   </nav>
@@ -15,6 +19,7 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import Dropdown from './Dropdown.vue'
+import DropdownItem from './DropdownItem.vue'
 export interface userProps{
   isLogin: boolean;
   name?: string;
@@ -22,7 +27,7 @@ export interface userProps{
 }
 export default defineComponent({
   name: 'GlobalHeader',
-  components: { Dropdown },
+  components: { Dropdown, DropdownItem },
   props: {
     user: {
       type: Object as PropType<userProps>,
