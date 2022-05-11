cd437ccb0153f42d59240998465ce9d9e5bddb5d
AA12GQ <875151567@qq.com>
2022-05-11 16:37:43 +0800

    ValidateInput 支持 v-model

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
      <form action="">
        <div class="mb-3">
       <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
-      <validate-input :rules="emailRules"></validate-input>
+      <validate-input :rules="emailRules" v-model="emailVal"></validate-input>
     </div>
     <div class="mb-3">
       <label for="exampleInputPassword1" class="form-label">密码</label>
@@ -15,7 +15,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 // import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { userProps } from './components/GlobalHeader.vue'
@@ -34,6 +34,7 @@ export default defineComponent({
     ValidateInput
   },
   setup () {
+    const emailVal = ref('bitepig')
     const emailRules: RulesProp = [
       { type: 'required', message: '电子邮箱地址不能为空' },
       { type: 'email', message: '请输入正确的电子邮箱格式' }
@@ -56,7 +57,8 @@ export default defineComponent({
       currentUser,
       emailRef,
       validateEmail,
-      emailRules
+      emailRules,
+      emailVal
     }
   }
 })
