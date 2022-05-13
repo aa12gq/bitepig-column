c2af3fe5a0566494325a73ee82fc06a65e813d47
AA12GQ <875151567@qq.com>
2022-05-13 20:18:56 +0800

    注册页面 和 首页 其他内容（静态布局实现

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 import ColumnDetail from '@/views/ColumnDetail.vue'
 import CreatePost from '@/views/CreatePost.vue'
+import Signup from '@/views/Signup.vue'
 const routerHistory = createWebHistory()
 const router = createRouter({
   history: routerHistory,
@@ -16,6 +17,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
     },
     {
       path: '/column/:id',
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="login-page">
-    <h5 class="my-4 text-center">登录咬楼猪账户</h5>
+  <div class="signup-page mx-auto p-3 w-330">
+    <h5 class="my-4 text-center">注册咬楼猪账户</h5>
     <validate-form @form-submit="onFormSubmit">
       <div class="mb-3">
         <label class="form-label">邮箱地址</label>
         <validate-input
-          ref="inputRef"
           type="text"
           placeholder="请输入邮箱地址"
           :rules="emailRules"
           v-model="emailVal"
+        ></validate-input>
+      </div>
+      <div class="mb-3">
+        <label class="form-label">昵称</label>
+        <validate-input
+          type="text"
+          placeholder="昵称"
+          :rules="nameRules"
+          v-model="nicknameVal"
         ></validate-input>
       </div>
       <div class="mb-3">
@@ -21,8 +29,17 @@
           v-model="passwordVal"
         ></validate-input>
       </div>
+      <div class="mb-3">
+        <label class="form-label">重复密码</label>
+        <validate-input
+          type="password"
+          placeholder="请再次输入密码"
+          :rules="repeatPasswordRules"
+          v-model="repeatPasswordVal"
+        ></validate-input>
+      </div>
       <template v-slot:submit>
-        <button type="submit" class="btn btn-primary btn-block btn-large">登录</button>
+        <button type="submit" class="btn btn-primary btn-block btn-large">注册新用户</button>
       </template>
     </validate-form>
   </div>
@@ -31,42 +48,48 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import { useRouter } from 'vue-router'
-import { useStore } from 'vuex'
 import ValidateInput, { RulesProps } from '@/base/ValidateInput.vue'
 import ValidateForm from '@/base/ValidateForm.vue'
 export default defineComponent({
-  name: 'Login',
+  name: 'Signup',
   components: {
     ValidateInput,
     ValidateForm
   },
   setup () {
-    const inputRef = ref<any>()
     const emailVal = ref('')
     const emailRules: RulesProps = [
       { type: 'required', message: '电子邮箱地址不能为空' },
       { type: 'email', message: '请输入正确的电子邮箱格式' }
     ]
+    const nicknameVal = ref('')
+    const nameRules: RulesProps = [
+      { type: 'required', message: '昵称不能为空' }
+    ]
     const passwordVal = ref('')
     const passwordRules: RulesProps = [
       { type: 'required', message: '密码不能为空' }
     ]
+    const repeatPasswordVal = ref('')
+    const repeatPasswordRules: RulesProps = [
+      { type: 'required', message: '重复密码不能为空' }
+    ]
     const router = useRouter()
-    const store = useStore()
-
     const onFormSubmit = (result: boolean) => {
       if (result) {
-        router.push('/')
-        store.commit('login')
+        router.push('/login')
       }
     }
     return {
-      emailRules,
       emailVal,
+      emailRules,
+      nicknameVal,
+      nameRules,
       passwordRules,
       passwordVal,
-      onFormSubmit,
-      inputRef
+      repeatPasswordVal,
+      repeatPasswordRules,
+      onFormSubmit
     }
   }
 })
