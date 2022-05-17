78d9eeeb3efaa52f24843da3252960fa7a99217d
AA12GQ <875151567@qq.com>
2022-05-17 10:10:33 +0800

    使用axios

---
+++ src/libs/http.ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = ''
+// interceptors 发送请求的拦截器
+axios.interceptors.request.use(config => {
+  config.params = {
+    ...config.params
+  }
+  return config
+})
+
+axios.get('/columns?currentPage=1&pageSize=5').then(res => {
+  console.log(res)
+})
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './libs/http'
 
 const app = createApp(App)
 app.use(router).use(router).use(store).mount('#app')
