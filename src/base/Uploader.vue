d4c982d3277c46fa5e51a7e1eb7c4c559a0924bc
AA12GQ <875151567@qq.com>
2022-06-17 23:11:23 +0800

    创建文章页面实现 Uploader 自定义样式

--- src/base/Uploader.vue
+++ src/base/Uploader.vue
@@ -1,6 +1,6 @@
-<template>
+4<template>
   <div class="file-upload">
-    <div class="file-upload-container" @click.prevent="triggerUpload">
+    <div class="file-upload-container" @click.prevent="triggerUpload" v-bind="$attrs">
       <slot v-if="fileStatus === 'loading'" name="loading">
         <button class="btn btn-primary" disabled>正在上传中...</button>
         </slot>
@@ -37,6 +37,7 @@ export default defineComponent({
     }
   },
   emits: ['file-uploaded', 'file-uploaded-error'],
+  inheritAttrs: false,
   setup (props, context) {
     const fileInput = ref<null | HTMLInputElement>(null)
     const fileStatus = ref<UploadStatus>('ready')
--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -3,12 +3,23 @@
     <h4>新建文章</h4>
     <uploader
     action="/api/upload"
-    :beforeUpload="beforeUpload"
-    @file-uploaded="onFileUploaded"
+    class="d-flex align-items-center justify-content-center bg-light text-secondary w-100 my-4"
     >
-    <template #uploaded="dataProps">
-      <img :src="dataProps.uploadedData.data.url" alt="" width="500">
-    </template>
+    <h2>点击上传头图</h2>
+    <template #loading>
+        <div class="d-flex">
+          <div class="spinner-border text-secondary" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+          <h2>正在上传</h2>
+        </div>
+      </template>
+        <template #uploaded="dataProps">
+        <div class="uploaded-area">
+          <img :src="dataProps.uploadedData.data.url" />
+          <h3>点击重新上传</h3>
+        </div>
+      </template>
     </uploader>
     <validate-form @form-submit="onFormSubmit" @change.prevent="handleFileChange">
       <div class="mb-3">
@@ -96,29 +107,41 @@ export default defineComponent({
         })
       }
     }
-    const beforeUpload = (file:File) => {
-      const isJPG = file.type === 'image/jpeg'
-      if (!isJPG) {
-        createMessage('上传图片只能是 JPG 格式', 'error')
-      }
-      return isJPG
-    }
-    const onFileUploaded = (rawData: ResponseType<ImageProps>) => {
-      createMessage(`上传图片ID ${rawData.data._id}`, 'success')
-    }
     return {
       titleVal,
       titleRules,
       contentVal,
       contentRules,
       onFormSubmit,
-      handleFileChange,
-      beforeUpload,
-      onFileUploaded
+      handleFileChange
     }
   }
 })
 </script>
 
-<style scoped>
+<style>
+.create-post-page .file-upload-container {
+  height: 200px;
+  cursor: pointer;
+  overflow: hidden;
+}
+.create-post-page .file-upload-container img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.uploaded-area {
+  position: relative;
+}
+.uploaded-area:hover h3 {
+  display: block;
+}
+.uploaded-area h3 {
+  display: none;
+  position: absolute;
+  color: #999;
+  text-align: center;
+  width: 100%;
+  top: 50%;
+}
 </style>
